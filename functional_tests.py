73c9435abdede748ccd3b15e34caf5e7fa543f2f
zjmvp <1016686103@qq.com>
2022-04-12 16:19:12 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,5 @@
+import time
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import unittest
 
@@ -5,13 +7,25 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome('./chromedriver.exe')
+
     def tearDown(self):
         self.browser.quit()
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.fail('Finish the text')
 
 
 if __name__ == "__main__":
-    unittest.main(warnings="ignore")	    
+    unittest.main(warnings="ignore")
